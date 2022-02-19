9d1851b46adbf4ad78fd7c69d234dc987f9b51bf
Viktoriya <step.vikulya@mail.ru>
2022-02-19 20:47:24 +0700

    Продолжение настройки webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
           filename: '[name].[contenthash].css'
         }),
         new HtmlWebpackPlugin({
-        template: "./src/index.html"
+        template: "./src/index.pug"
     })],
     module: {
         rules: [
@@ -53,6 +53,10 @@ module.exports = {
             {
               test: /\.(woff|woff2|eot|ttf|otf)$/i,
               type: 'asset/resource',
+            },
+            {
+              test: /\.pug$/,
+              loader: 'pug-loader',
             },
         ]
     }
